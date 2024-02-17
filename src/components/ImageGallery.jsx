7096bc1db1769f6ff780a3049ad5f23eb1f8b8ec
Martyna00038 <martyna00038@gmail.com>
2024-02-17 15:48:36 +0100

    hw3 v271

---
+++ src/components/ImageGallery.jsx
@@ -0,0 +1,32 @@
+import { Component } from 'react';
+import ImageGalleryItem from './ImageGalleryItem';
+import css from './gallery.module.css';
+import PropTypes from 'prop-types';
+
+export default class ImageGallery extends Component {
+  render() {
+    const { images, onClick } = this.props;
+
+    return (
+      <ul className={css.gallery}>
+        {images.map(image => (
+          <ImageGalleryItem
+            key={image.id}
+            image={image}
+            onClick={() => onClick(image.largeImageURL)}
+          />
+        ))}
+      </ul>
+    );
+  }
+}
+
+ImageGallery.propTypes = {
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      largeImageURL: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onClick: PropTypes.func.isRequired,
+};
